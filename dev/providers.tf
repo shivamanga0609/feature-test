081ca0ad626a755b0ca5d182b99299033891ace7
shivamanga0609 <114624074+shivamanga0609@users.noreply.github.com>
2022-10-07 10:46:47 -0500

    Update providers.tf

--- dev/providers.tf
+++ dev/providers.tf
@@ -1,13 +1,7 @@
 provider "azurerm" {
   features {}
-}
-
-terraform {
-  required_version = ">= 1.0.0"
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "2.64.0"
-    }
-  }
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
