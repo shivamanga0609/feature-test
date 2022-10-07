1fdd588b527cda42418290c9db4c62375aa6fc33
shivamanga0609 <114624074+shivamanga0609@users.noreply.github.com>
2022-10-07 11:35:30 -0500

    Create providers.tf

---
+++ providers.tf
@@ -0,0 +1,7 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "5740e3cd-aee2-48a2-bafe-098b39cdac9b"
+  client_id       = "f5df5aaf-8a3f-4b5b-80ab-339078d827c4"
+  client_secret   = "c73bde38-4636-4b76-9338-d642e121ef64"
+  tenant_id       = "5bf7f43b-c6cb-4796-beef-1bada091eefc"
+}
