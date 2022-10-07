9f082bf0d8c634ac130c814a0d22df6ccf4720c7
shivamanga0609 <114624074+shivamanga0609@users.noreply.github.com>
2022-10-07 17:55:11 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,7 +1,21 @@
 provider "azurerm" {
-  features {}
-  subscription_id = "5740e3cd-aee2-48a2-bafe-098b39cdac9b"
-  client_id       = "f5df5aaf-8a3f-4b5b-80ab-339078d827c4"
-  client_secret   = "nWK8Q~DGaVG-nmCjKnPgQCT4ipTIxgbixpkXhbrs"
-  tenant_id       = "5bf7f43b-c6cb-4796-beef-1bada091eefc"
+  terraform {
+    required_version = ">= 1.0.0"
+    required_providers {
+         azurerm = {
+             source  = "hashicorp/azurerm"
+             version = "2.64.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.0"
+    }
+  }
+}
+    features {
+        subscription_id = "5740e3cd-aee2-48a2-bafe-098b39cdac9b"
+         client_id       = "f5df5aaf-8a3f-4b5b-80ab-339078d827c4"
+         client_secret   = "nWK8Q~DGaVG-nmCjKnPgQCT4ipTIxgbixpkXhbrs"
+         tenant_id       = "5bf7f43b-c6cb-4796-beef-1bada091eefc"
+    }   
 }
