47c1ee81f194a60b0332bf337bbac68c3595998e
shivamanga0609 <114624074+shivamanga0609@users.noreply.github.com>
2023-04-08 21:30:13 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -13,9 +13,7 @@ provider "azurerm" {
   }
 }
     features {
-        subscription_id = "5740e3cd-aee2-48a2-bafe-098b39cdac9b"
-         client_id       = "f5df5aaf-8a3f-4b5b-80ab-339078d827c4"
-         client_secret   = "nWK8Q~DGaVG-nmCjKnPgQCT4ipTIxgbixpkXhbrs"
-         tenant_id       = "5bf7f43b-c6cb-4796-beef-1bada091eefc"
+        subscription_id = "211978a0-4bca-46a2-b47a-daa2d97ee3ce
+
     }   
 }
