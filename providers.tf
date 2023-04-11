d426d64cef51f1da4af2bbb90d0927c9eaefc872
shivamanga0609 <114624074+shivamanga0609@users.noreply.github.com>
2023-04-11 12:04:56 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,19 +1,24 @@
-//provider "azurerm" {
-//  terraform {
-//   required_version = ">= 1.0.0"
-//    required_providers {
-//         azurerm = {
-//             source  = "hashicorp/azurerm"
-//             version = "2.64.0"
-//    }
-//    random = {
-//      source  = "hashicorp/random"
-//      version = ">= 3.0"
-//    }
-//  }
-//}
-//    enviornment {
-//        AZURE_SUBSCRIPTION_ID = "211978a0-4bca-46a2-b47a-daa2d97ee3ce"
-//
-//    }   
-//}
+provider "azurerm" {
+  terraform {
+   required_version = ">= 1.0.0"
+    required_providers {
+         azurerm = {
+             source  = "hashicorp/azurerm"
+             version = "2.64.0"
+    }
+    environment {
+      variables = {
+       ARM_SUBSCRIPTION_ID = "211978a0-4bca-46a2-b47a-daa2d97ee3ce"
+   }
+ }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.0"
+    }
+  }
+}
+#    enviornment {
+#        AZURE_SUBSCRIPTION_ID = "211978a0-4bca-46a2-b47a-daa2d97ee3ce"
+
+    }   
+}
